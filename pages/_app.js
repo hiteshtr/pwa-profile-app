072b7098a4eeacc7735a1e2079230594f05c442b
Hitesh Trivedi <hiteshtr@gmail.com>
2021-07-10 10:28:20 +0530

    Moved to tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,7 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-import theme from '../styles/theme';
+import 'tailwindcss/tailwind.css'
 
-const cache = createCache({ key: 'css' });
-cache.compat = true;
-// import '../styles/globals.css'
-
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-  return (
-    <CacheProvider value={cache}>
-      <Head>
-        <title>My page</title>
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+export default MyApp
