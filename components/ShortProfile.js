1f26c8cb89de8f7c0b1d9465e7f52cdb29357fdb
Hitesh Trivedi <hiteshtr@gmail.com>
2021-07-10 12:26:53 +0530

    added cards

--- components/ShortProfile.js
+++ components/ShortProfile.js
@@ -1,37 +1,24 @@
+import Link from 'next/link'
+
 export function ShortProfile() {
   return (
-    <div className="bg-white my-12 pb-6 w-full justify-center items-center overflow-hidden md:max-w-sm rounded-lg shadow-sm mx-auto">
-      <div className="relative h-40">
-        <img className="absolute h-full w-full object-cover" src="https://images.unsplash.com/photo-1448932133140-b4045783ed9e?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"/>
-      </div>
-      <div className="relative shadow mx-auto h-24 w-24 -my-12 border-white rounded-full overflow-hidden border-4">
-        <img className="object-cover w-full h-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80"/>
-      </div>
-      <div className="mt-16">
-        <h1 className="text-lg text-center font-semibold">
-          Cassie 
-        </h1>
-        <p className="text-sm text-gray-600 text-center">
-          13 connections in common
-        </p>
-      </div>
-      <div className="mt-6 pt-3 flex flex-wrap mx-6 border-t">
-        <div className="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default">
-          User experience
+    <div className="card text-center shadow-2xl">
+      <figure className="px-10 pt-10">
+        <img src="https://picsum.photos/id/1005/400/250" className="rounded-xl"/>
+      </figure> 
+      <div className="card-body">
+        <h2 className="card-title">shadow, center, padding</h2> 
+        <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus veritatis sed molestiae voluptates incidunt iure sapiente.</p> 
+        <div className="card-actions">
+          <div className="badge badge-ghost">Article</div> 
+          <div className="badge badge-ghost">Photography</div>
         </div>
-        <div className="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default">
-          VueJS
-        </div>
-        <div className="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default">
-          TailwindCSS
-        </div>
-        <div className="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default">
-          React
-        </div>
-        <div className="text-xs mr-2 my-1 uppercase tracking-wider border px-2 text-indigo-600 border-indigo-600 hover:bg-indigo-600 hover:text-indigo-100 cursor-default">
-          Painting
+        <div className="justify-center card-actions">
+          <Link href="/profile">
+            <button className="btn btn-outline btn-accent">More info</button>
+          </Link>
         </div>
       </div>
-    </div>
+    </div> 
   );
 }
